5fb2d1308e36963abada6b4665ecda474d84a0c9
Tanakorn <tanakorn.paiboon@gmail.com>
2020-08-27 22:39:04 +0700

    membership_op

---
+++ membership_op.py
@@ -0,0 +1,17 @@
+# flowers = "lily","rose","daisy","tulip"
+# if "rose" in flowers:
+#     print("available")
+# else:
+#     print("not available")
+#
+# sentencee = "over the rainbow"
+# if "rain" in sentencee:
+#     print("ok")
+# else:
+#     print("not ok")
+
+n= 10,8,,16,15,30,60
+if 15 in n:
+    print("ok")
+else:
+    print("not ok")
